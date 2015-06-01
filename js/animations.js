83d12d6777b42688ee9a4ab6bc71746335c8230d
Zane Swafford <zane@zaneswafford.com>
2015-06-01 18:13:32 -0500

    Updated Personal Site with new theme

--- js/animations.js
+++ js/animations.js
@@ -1,48 +1,110 @@
-$(document).ready(bindEvents);
+var typableTexts = ["Hello, I'm Zane.^200 I live\n in Kansas.^200 \nI make things with^100 <em>❤︎</em>.",
+                "I am available for hire at <a target='_blank' href='http://developedinkansas.com/'>Developed In Kansas</a>.^300 You can find my open-source work on <a target='_blank' href='https://github.com/zaneswafford'>GitHub</a>.^300 You can email me at <a href='mailto:&#122;&#097;&#110;&#101;&#064;&#111;&#116;&#116;&#101;&#114;&#115;&#046;&#105;&#111;'>zane@otters.io</a>.^300 <br><br>My recent doings:<ul><li><a target='_blank' href='http://otters.io'>otters.io</a></li><li><a target='_blank' href='https://github.com/zaneswafford/wwdc-scholarship-app-2015'>My WWDC Scholarship Application</a></li><li><a target='_blank' href='http://dave.zaneswafford.com'>Never Enough Dave</a></li><li><a target='_blank' href='http://mom.zaneswafford.com'>A Mother's Day Card</a></li></ul>",
+                "I enjoy:<ul><li>Tinkering with my tiny car</li><li>Brewing coffee</li><li>Playing with old Macs</li><li>Blogging at <a target='_blank' href='http://otters.io'>otters.io</a></li><li>Tweeting from <a target='_blank' href='https://twitter.com/ZaneSwafford'>@zaneswafford</a></li><li>Puppies</li>", 
+                "Available as a <a target='_blank' href='doc/zane-swafford-resume.docx'>Word doc</a> or <a target='_blank' href='doc/zane-swafford-resume.pdf'>PDF</a>."];
+
+$(document).ready(function() {
+  bindEvents();
+  showBody();
+});
+
+function showBody() {
+  $("body").fadeIn(600, function(){
+    loadPageFromCurrentUrl();
+    typeTextInElement(typableTexts[0], $("p:first-of-type"));
+  }).css("display","inline-block");
+}
 
 function bindEvents() {
-	$("a.listChild").click(scrollToLinkedSection);
-	$("img.transitionable").hover(hoverImgTransition);
-	$("a.transitionable").hover(hoverLinkTransition);
+  $("a").click(onNavLinkClicked);
 }
 
-function scrollToLinkedSection() {
-	section = $(this).find("li").text().toLowerCase();
-	$('html, body').animate({
-		scrollTop: $("#" + section).offset().top-10
-	}, 800);
+function typeTextInElement(text, element) {
+  $(element).typed({
+    strings: [text],
+    typeSpeed: 0,
+    startDelay: 400,
+    showCursor: false
+  });
 }
 
-function hoverLinkTransition() {
-	imgArea = $("#" + $(this).attr("id").slice(0,-4));
+function onNavLinkClicked() {
+  var clickedLinkIndex = $(this).parent("li").index()+1;
+  var visiblePIndex = $("p:visible").first().index()-2;
+  var clickedLink = $(this);
+  var linkText = $(this).text();
+  $("p").stop().filter(function(){ return ! $(this).is(":hidden"); }).fadeOut(250, function() {
+    if (clickedLinkIndex != visiblePIndex) {
+      $("p").eq(clickedLinkIndex).stop().fadeIn(250).css("display","inline-block");;
+      setLinkAsInactive("a")
+      setLinkAsActive(clickedLink);
+      typeTextInElement(typableTexts[clickedLinkIndex], $("p").eq(clickedLinkIndex));
+      
+      if (linkText === "Zane Swafford ❤") {
+        resetURL();
+      } else {
+        pushUrlSuffixWithTitleSuffix(linkText.substring(3).toLowerCase(), linkText.substring(3));
+      }
 
-	if($(this).attr("id").slice(0, -5) === "msg") {
-		imgArea = $("#msg");
-	}
-
-	if ($(this).hasClass('active')) {
-		$(this).removeClass('active');
-		imgArea.removeClass('active');
-	} else {
-		$(this).addClass('active');
-		imgArea.addClass('active');
-	}
-
+    } else {
+      setLinkAsInactive(clickedLink);
+      typeTextInElement(typableTexts[0], $("p:first-of-type"));
+      resetURL();
+    }
+  });
 }
 
+function setLinkAsActive(link) {
+  $(link).css("color", "#f8584e");
+}
 
-function hoverImgTransition() {
-	textArea = $("#" + $(this).attr("id") + "Text");
+function setLinkAsInactive(link) {
+  $(link).css("color", "#228DFF");
+}
 
-	if($(this).attr("id") === "msg") {
-		textArea = $("#msgText1, #msgText2, #msgText3");
-	}
+function pushUrlSuffixWithTitleSuffix(urlSuffix, titleSuffix) {
+  var url = window.location.href.split("#")[0];
+  window.history.pushState(null, "Zane Swafford | " + titleSuffix, url + "#" + urlSuffix); 
+}
 
-	if ($(this).hasClass('active')) {
-		$(this).removeClass('active');
-		textArea.removeClass('active');
-	} else {
-		$(this).addClass('active');
-		textArea.addClass('active');
-	}
+function resetURL() {
+  var url = window.location.href;
+  if (url.indexOf("#") > -1) {
+    url = url.split("#")[0];
+  }
+
+  window.history.pushState(null, "Zane Swafford", url); 
+}
+
+function loadPageFromCurrentUrl() {
+  var urlParts = window.location.href.split("#");
+  var urlEnd = urlParts[urlParts.length-1];
+  switch (urlEnd) {
+    case "work":
+      $("p").stop().fadeOut(250, function() {
+        $("p").eq(1).stop().fadeIn(250).css("display","inline-block");;
+        setLinkAsInactive("a")
+        setLinkAsActive($("header ul li a").eq(0));
+        typeTextInElement(typableTexts[1], $("p").eq(1));
+      });
+      break;
+    case "play":
+      $("p").stop().fadeOut(250, function() {
+        $("p").eq(2).stop().fadeIn(250).css("display","inline-block");;
+        setLinkAsInactive("a")
+        setLinkAsActive($("header ul li a").eq(1));
+        typeTextInElement(typableTexts[2], $("p").eq(2));
+      });
+      break;
+    case "resume":
+      $("p").stop().fadeOut(250, function() {
+        $("p").eq(3).stop().fadeIn(250).css("display","inline-block");;
+        setLinkAsInactive("a")
+        setLinkAsActive($("header ul li a").eq(2));
+        typeTextInElement(typableTexts[3], $("p").eq(3));
+      });
+      break;
+    default:
+      break;
+  }
 }
