a127cf8f9c092d3dc0b4ae5bbf01c93760601f26
Zane Swafford <zane@zaneswafford.com>
2015-08-13 14:25:24 -0600

    Added Just TicTacToe link

--- js/animations.js
+++ js/animations.js
@@ -1,5 +1,5 @@
 var typableTexts = [ "Hello, I'm Zane.^100 I live\n in Kansas.^100 \nI make things with^100 <em>❤︎</em>.",
-               	 	 "I am available for hire at <a id='adik' class='black'>Developed In Kansas</a>.^100 You can find my open-source work on <a id='agh' class='black'>GitHub</a>.^100 You can email me at <a id='amt' class='black'>zane@otters.io</a>.^100 <br><br>My recent doings:<ul><li><a id='aoio' class='black'>otters.io</a></li><li><a id='awwdc' class='black'>My WWDC Scholarship App</a></li><li><a id='adave' class='black'>Never Enough Dave</a></li><li><a id='amom' class='black'>A Mother's Day Card</a></li></ul>",
+               	 	 "I am available for hire at <a id='adik' class='black'>Developed In Kansas</a>.^100 You can find my open-source work on <a id='agh' class='black'>GitHub</a>.^100 You can email me at <a id='amt' class='black'>zane@otters.io</a>.^100 <br><br>My recent doings:<ul><li><a id='aoio' class='black'>otters.io</a></li><li><a id='ajttt' class='black'>Just Tic-Tac-Toe</a></li><li><a id='awwdc' class='black'>My WWDC Scholarship App</a></li><li><a id='adave' class='black'>Never Enough Dave</a></li><li><a id='amom' class='black'>A Mother's Day Card</a></li></ul>",
                 	 "I enjoy:<ul><li>Tinkering with my tiny car</li><li>Brewing coffee</li><li>Playing with old Macs</li><li>Blogging at <a id='ablog' class='black'>otters.io</a></li><li>Tweeting from <a id='atwt' class='black'>@zaneswafford</a></li><li>Puppies</li>", 
                 	 "Available as a <a id='aresd' class='black'>Word doc</a> or <a id='aresp' class='black'>PDF</a>." ];
 
@@ -11,6 +11,7 @@ var linksForTexts = { "adik" : "http://developedinkansas.com/",
 					  "adave" : "http://dave.zaneswafford.com",
 					  "amom" : "http://mom.zaneswafford.com",
 					  "ablog" : "http://otters.io",
+					  "ajttt" : "http://tictactoe.zaneswafford.com",
 					  "atwt" : "https://twitter.com/ZaneSwafford",
 					  "aresd" : "doc/zane-swafford-resume.docx",
 					  "aresp" : "doc/zane-swafford-resume.pdf" };
